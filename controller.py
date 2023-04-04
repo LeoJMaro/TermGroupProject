6888bf5bdb865a08b2ce06d43ddd25214e13d2d4
Jason <jasongtse@gmail.com>
2023-04-04 20:59:42 -0230

    Modified function for 'increase inventory', added functions for 'decrease inventory' and 'get product by ID'

--- controller.py
+++ controller.py
@@ -10,17 +10,17 @@ def add_customer(customer_first_name, customer_last_name, customer_phone, custom
     sql = f"INSERT INTO programming_group_project.customers VALUES (default, '{customer_first_name}', '{customer_last_name}', '{customer_phone}', '{customer_email}', '{customer_address}');"
     executeQueryAndCommit(sql)
 
-def increase_inventory(inventory_stock):
-    sql = f"UPDATE programming_group_project.products SET inventory_stock = {inventory_stock};"
+def increase_inventory(intake_amount):
+    sql = f"UPDATE programming_group_project.products SET inventory_stock = inventory_stock + {intake_amount};"
     executeQueryAndCommit(sql)
 
+def decrease_inventory(purchase_amount):
+    sql = f"UPDATE programming_group_project.products SET inventory_stock = inventory_stock - {purchase_amount};"
+    executeQueryAndCommit(sql)
 
-def decrease_inventory():
-    pass
-
-def get_product_by_id():
-    pass
-
+def get_product_by_id(product_id):
+    sql = f"SELECT product_name, product_description, inventory_stock, price FROM programming_group_project WHERE product_id = {product_id};"
+    executeQueryAndReturnResult(sql)
 
 def show_products():
     sql = "SELECT * FROM products"
