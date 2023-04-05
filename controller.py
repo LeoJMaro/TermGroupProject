61097e3970be049213c6d536f8b9d7751aa9cfe4
Jason <jasongtse@gmail.com>
2023-04-04 21:47:58 -0230

    Altered parameters of 'create invoice' to match streamlined database

--- controller.py
+++ controller.py
@@ -1,8 +1,8 @@
 from sql_functions import executeQueryAndCommit, executeQueryAndReturnResult
 
 
-def add_invoice(vendor_id, invoice_number, invoice_date, invoice_total, payment_total, invoice_due_date, payment_date):
-    sql = f"INSERT INTO programming_group_project.invoices VALUES (default, {vendor_id}, '{invoice_number}', {invoice_date}, {invoice_total}, {payment_total}, {invoice_due_date}, {payment_date});"
+def add_invoice(invoice_number, customer_id, invoice_date):
+    sql = f"INSERT INTO programming_group_project.invoices VALUES ('{invoice_number}', {customer_id}, {invoice_date});"
     return executeQueryAndCommit(sql)
 
 
