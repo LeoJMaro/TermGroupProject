a0e9f5dd8d000a285d17dbbe60116185d230d4a7
Jason <jasongtse@gmail.com>
2023-04-05 16:29:28 -0230

    Optimized increase and decrease stock functions by allowing the product being updated to be specified by ID

--- controller.py
+++ controller.py
@@ -1,26 +1,26 @@
 from sql_functions import executeQueryAndCommit, executeQueryAndReturnResult
 
 
-def add_invoice(invoice_number, customer_id, invoice_date):
-    sql = f"INSERT INTO programming_group_project.invoices VALUES ('{invoice_number}', {customer_id}, {invoice_date});"
+def add_invoice(customer_id, invoice_date):
+    sql = f"INSERT INTO programming_group_project.invoices VALUES (DEFAULT, {customer_id}, {invoice_date});"
     return executeQueryAndCommit(sql)
 
 
 def add_customer(customer_first_name, customer_last_name, customer_phone, customer_email, customer_address):
-    sql = f"INSERT INTO programming_group_project.customers VALUES (default, '{customer_first_name}', '{customer_last_name}', '{customer_phone}', '{customer_email}', '{customer_address}');"
-    executeQueryAndCommit(sql)
+    sql = f"INSERT INTO programming_group_project.customers VALUES (DEFAULT, '{customer_first_name}', '{customer_last_name}', '{customer_phone}', '{customer_email}', '{customer_address}');"
+    return executeQueryAndCommit(sql)
 
-def increase_inventory(intake_amount):
-    sql = f"UPDATE programming_group_project.products SET inventory_stock = inventory_stock + {intake_amount};"
-    executeQueryAndCommit(sql)
+def increase_inventory(intake_amount, product_id):
+    sql = f"UPDATE programming_group_project.products SET inventory_stock = inventory_stock + {intake_amount} WHERE product_id = {product_id};"
+    return executeQueryAndCommit(sql)
 
-def decrease_inventory(purchase_amount):
-    sql = f"UPDATE programming_group_project.products SET inventory_stock = inventory_stock - {purchase_amount};"
-    executeQueryAndCommit(sql)
+def decrease_inventory(purchase_amount, product_id):
+    sql = f"UPDATE programming_group_project.products SET inventory_stock = inventory_stock - {purchase_amount} WHERE product_id = {product_id};"
+    return executeQueryAndCommit(sql)
 
 def get_product_by_id(product_id):
     sql = f"SELECT product_name, product_description, inventory_stock, price FROM programming_group_project WHERE product_id = {product_id};"
-    executeQueryAndReturnResult(sql)
+    return executeQueryAndReturnResult(sql)
 
 def show_products():
     sql = "SELECT * FROM products"
