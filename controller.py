fc8df4914685d390f331ef03a30b229dd392e8e1
Jason <jasongtse@gmail.com>
2023-04-05 15:26:57 -0230

    Added a function to display invoices by ID, total cost, cost breakdown and products included, and a function to search invoices by customer ID

--- controller.py
+++ controller.py
@@ -26,4 +26,12 @@ def show_products():
     sql = "SELECT * FROM products"
     return executeQueryAndReturnResult(sql)
 
+def search_invoice_by_customer_id(customer_id):
+    sql = f"SELECT ip.invoice_id AS 'Invoice ID', SUM(ip.product_quantity * p.price) AS 'Total Cost' FROM invoice_products AS ip JOIN products AS p ON ip.product_id = p.product_id JOIN invoices AS i ON ip.invoice_id = i.invoice_id WHERE i.customer_id = {customer_id} GROUP BY ip.invoice_id;"
+    return executeQueryAndReturnResult(sql)
+
+def total_cost_breakdown_and_products_per_invoice():
+    sql = f"SELECT ip.invoice_id AS 'Invoice ID',SUM(ip.product_quantity * p.price) AS 'Total Cost', GROUP_CONCAT(ip.product_quantity * p.price) AS 'Cost Per Product', GROUP_CONCAT(p.product_id) AS 'ID Numbers of Products Included' FROM invoice_products AS ip JOIN products AS p ON ip.product_id = p.product_id JOIN invoices AS i ON ip.invoice_id = i.customer_id GROUP BY ip.invoice_id;"
+    return executeQueryAndReturnResult(sql)
+
 # add_invoice(69,'aaa111', 'NOW()', 299.99, 299.99, 'NOW()', 'NOW()')
