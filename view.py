b49f7e7befd598676d6b13938fd39e11c2618f8a
Noah Forward <forwardnoah@gmail.com>
2023-04-06 20:49:34 -0230

    Added placeholder options to the text GUI for future functionality

--- view.py
+++ view.py
@@ -3,6 +3,7 @@ from controller import *
 
 # Placeholder view
 products_table = PrettyTable()
+current_product_ids = []
 
 
 def initialize_tables():
@@ -16,6 +17,9 @@ def display_options():
     print('=' * 50)
     print('0. Exit')
     print('1. Start Transaction ')
+    print('2. Show Products')
+    print('3. Show Out Of Stock Products')
+    print('4. Show Last Month Customer History')
     print('=' * 50)
 
 
@@ -43,15 +47,21 @@ if __name__ == '__main__':
                     user_choice = input('Enter a choice: ')
                     match user_choice:
                         case '0':
+                            # Create invoice in database here with add_invoice
                             break
                         case '1':
                             print(products_table)
                         case '2':
                             print(products_table)
                             user_product_choice = input('Choose product: ')
-
                         case '3':
                             pass
 
             case '2':
+                print(products_table)
+            case '3':
+                pass
+            case '4':
+                pass
+            case '5':
                 pass
