0627e5d8f420ba1ff3ee29d76f0f52fced3350bb
Joel <leojmaro@gmail.com>
2023-04-07 20:35:04 -0230

    Add more to Veiw

--- view.py
+++ view.py
@@ -42,29 +42,23 @@ def homepage():
     widgets["buttons"]["customer"] = customer
     grid.addWidget(customer, 2, 0)
 def inventory_page():
-    row = 0
     table = QTableWidget()
-    table.column()
-    table.insertRow()
-    table.insertColumn()
-
+    table.height()
+    table.width()
     grid.addWidget(table, 0, 0)
-
+    table.setColumnCount(6)
     datos = controller.show_products()[1]
-    for endian in datos:
-        table.setRowCount(row + 1)
+    for row, endian in enumerate(datos):
 
         idDato = QTableWidgetItem(endian[0])
         idDato.setTextAlignment(4)
-
+        table.insertRow(row)
         table.setItem(row, 0, idDato)
         table.setItem(row, 1, QTableWidgetItem(endian[1]))
         table.setItem(row, 2, QTableWidgetItem(endian[2]))
         table.setItem(row, 3, QTableWidgetItem(endian[3]))
         table.setItem(row, 4, QTableWidgetItem(endian[4]))
         table.setItem(row, 5, QTableWidgetItem(str(endian[5])))
-
-        row += 1
 
 inventory_page()
 
